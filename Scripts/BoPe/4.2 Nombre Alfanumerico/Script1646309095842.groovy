7368c980ff7362aa8159f2b5e6b51d19f86034b0
andrese19 <andrese19@hotmail.com>
2022-03-03 09:14:08 -0300

    Test Case Campo NOMBRE/ APELLIDO - Datos  Alfanumericos

---
+++ Scripts/BoPe/4.2 Nombre Alfanumerico/Script1646309095842.groovy
@@ -0,0 +1,53 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('http://10.1.0.18:3000/')
+
+WebUI.maximizeWindow()
+
+WebUI.waitForPageLoad(3)
+
+WebUI.selectOptionByIndex(findTestObject('URL/Formulario BoPe/SELECT_provincia'), 8)
+
+WebUI.selectOptionByIndex(findTestObject('URL/Formulario BoPe/SELECT_cel'), 2)
+
+WebUI.setText(findTestObject('URL/Formulario BoPe/SET_nombre'), 'Andres0')
+
+WebUI.setText(findTestObject('URL/Formulario BoPe/SET_apellidos'), 'Cardona')
+
+WebUI.selectOptionByIndex(findTestObject('URL/Formulario BoPe/SELECT_sexo'), 2)
+
+WebUI.selectOptionByIndex(findTestObject('URL/Formulario BoPe/SELECT_tipoDOC'), 2)
+
+WebUI.setText(findTestObject('URL/Formulario BoPe/SET_numeroDOC'), '95816350')
+
+WebUI.setText(findTestObject('URL/Formulario BoPe/SET_email'), 'cardona@cc.com')
+
+WebUI.click(findTestObject('URL/Formulario BoPe/BOTON_generar'))
+
+WebUI.scrollToPosition(0, 400)
+
+SC = WebUI.takeScreenshot()
+
+WebUI.takeScreenshot('C:\\Users\\Andres Torrealba\\OneDrive\\Escritorio\\KEYLAB\\EVIDENCIA\\4.2SC-NombreAlfanumerico.png')
+
+WebUI.delay(2)
+
+WebUI.closeBrowser()
+
